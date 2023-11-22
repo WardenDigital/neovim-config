b95f254de7d4767efaa14e802163e2a9d4e0fe15
Andrew Konoplia <vanillaquila@gmail.com>
2023-11-22 12:46:00 +0200

    expose hidden files

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,7 +1,14 @@
 require("neo-tree").setup({
-  window = {
-    mappings = {
-      ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+    filesystem = {
+        filtered_items = {
+            visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+            hide_dotfiles = false,
+            hide_gitignored = true,
+        },
+    },
+    window = {
+        mappings = {
+            ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = true } },
+        }
     }
-  }
 })
