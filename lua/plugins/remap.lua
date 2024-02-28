31372dea277d76c2cdb31692ca84d9d3896f1171
WardenDigital <wardendigital@proton.me>
2024-02-29 01:54:55 +0200

    remap of esc in insert mod

--- lua/plugins/remap.lua
+++ lua/plugins/remap.lua
@@ -8,3 +8,9 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 -- Ctrl + C 
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+
+-- Esc to normal mode remap
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+vim.keymap.set("i", "jj", "<Esc>")
+vim.keymap.set("i", "kk", "<Esc>")
