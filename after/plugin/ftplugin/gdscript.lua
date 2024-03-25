085423aee0e808e6076bfbd779172331c14cf9e1
warden_digital <wardendigital@proton.me>
2024-03-25 22:17:49 +0200

    added comment for Godot editor setup

--- after/plugin/ftplugin/gdscript.lua
+++ after/plugin/ftplugin/gdscript.lua
@@ -2,6 +2,7 @@ local lspconfig = require("lspconfig")
 lspconfig.gdscript.setup({})
 
 local pipepath = vim.fn.stdpath("cache") .. "/server.pipe"
+-- set Godot External Text Editor command to "nvim" and flag to "--server /home/warden-home/.cache/nvim/server.pipe --remote-send "<C-\><C-N>:n {file}<CR>:call cursor({line},{col})<CR>"
 if not vim.loop.fs_stat(pipepath) then
 	vim.fn.serverstart(pipepath)
 end
