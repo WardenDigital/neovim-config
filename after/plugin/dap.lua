262c49054049d9687aa0f659edc435163d595512
Andrew.Konoplia <andrew.konoplia@U-3KYQUJ3769J1N.enragroup.local>
2024-04-11 20:19:50 +0300

    added dap

---
+++ after/plugin/dap.lua
@@ -0,0 +1,28 @@
+local dap, dapui = require("dap"), require("dapui")
+dapui.setup()
+
+vim.api.nvim_set_keymap("n", "<leader>dt", ":lua require('dapui').toggle()<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dd", ":lua require('dap').toggle_breakpoint()<CR>", { noremap = true })
+vim.api.nvim_set_keymap(
+	"n",
+	"<leader>ddif",
+	":lua require('dap').set_breakpoint(vim.fn.input('Breakpoint condition: '))<CR>",
+	{ noremap = true }
+)
+vim.api.nvim_set_keymap("n", "<leader>dc", ":DapContinue<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<leader>dr", ":lua require('dapui').open({reset = true})<CR>", { noremap = true })
+
+dap.adapters.php = {
+	type = "executable",
+	command = "node",
+	args = { os.getenv("HOME") .. "/Soft/vscode-php-debug/out/phpDebug.js" },
+}
+
+dap.configurations.php = {
+	{
+		type = "php",
+		request = "launch",
+		name = "Listen for xdebug",
+		port = 9003,
+	},
+}
--- lua/plugins/lazy.lua
+++ lua/plugins/lazy.lua
@@ -109,6 +109,11 @@ local plugins = {
 			{ "j-hui/fidget.nvim", opts = {} },
 		},
 	},
+	-- DAP
+
+	{ "rcarriga/nvim-dap-ui", dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } },
+	{ "nvim-telescope/telescope-dap.nvim" },
+	{ "theHamsta/nvim-dap-virtual-text" },
 	-- Autoformat
 	{
 		"stevearc/conform.nvim",
