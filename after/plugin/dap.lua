4c1796178bb2d4c8f089df7c321fa3c3e5f69ba0
Andrew.Konoplia <andrew.konoplia@U-3KYQUJ3769J1N.enragroup.local>
2024-04-12 10:33:45 +0300

    dapui event triggers

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -26,3 +26,16 @@ dap.configurations.php = {
 		port = 9003,
 	},
 }
+
+dap.listeners.before.attach.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.launch.dapui_config = function()
+	dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
+	dapui.close()
+end
+dap.listeners.before.event_exited.dapui_config = function()
+	dapui.close()
+end
