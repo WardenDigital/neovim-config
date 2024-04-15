b1a634b3a632aaa7ef931b9244e4cb40dd728831
Andrew.Konoplia <andrew.konoplia@U-3KYQUJ3769J1N.enragroup.local>
2024-04-16 01:26:46 +0300

    telescope rework
    Now has:
    Find All - search for all files including ignored;
    Find Word - Live Grep for word under cursor
    -- In Search: --
    Ctrl + i - adds no-ignore to searched phrase
    Ctrl + q - Quotates searched query
    -- In visual --
    Ctrl + f - find visually selected text

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,9 +1,38 @@
 local builtin = require("telescope.builtin")
 local telescope = require("telescope")
-require("telescope").load_extension("dap")
+local lga_actions = require("telescope-live-grep-args.actions")
+local lga_shortcuts = require("telescope-live-grep-args.shortcuts")
+
+telescope.setup({
+	extensions = {
+		live_grep_args = {
+			auto_quoting = true,
+			mappings = {
+				i = {
+					["<C-q>"] = lga_actions.quote_prompt(),
+					["<C-i>"] = lga_actions.quote_prompt({ postfix = " --no-ignore " }),
+				},
+			},
+		},
+	},
+})
+
+telescope.load_extension("dap")
+telescope.load_extension("media_files")
+telescope.load_extension("live_grep_args")
 
 vim.keymap.set("n", "<leader>fm", telescope.extensions.media_files.media_files, { desc = "[F]ind [M]edia-files" })
 vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [G]rep" })
+vim.keymap.set("n", "<leader>fa", function()
+	builtin.find_files({ no_ignore = true })
+end, { desc = "[F]ind [A]ll" })
+vim.keymap.set("n", "<leader>fg", telescope.extensions.live_grep_args.live_grep_args, { desc = "[F]ind [G]rep" })
+vim.keymap.set("n", "<leader>fw", function()
+	lga_shortcuts.grep_word_under_cursor({ quote = false, postfix = "" })
+end, { desc = "[F]ind [W]ord" })
+vim.keymap.set("v", "<C-f>", function()
+	lga_shortcuts.grep_visual_selection({ quote = false, postfix = "" })
+end, { desc = "[F]ind in visual mode" })
+vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "[F]ind [B]uffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "[F]ind [H]elp-tags" })
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,12 +3,13 @@ return {
 	tag = "0.1.5",
 	dependencies = {
 		{ "nvim-lua/popup.nvim" },
+		{
+			"nvim-telescope/telescope-live-grep-args.nvim",
+			version = "^1.0.0",
+		},
 		{ "nvim-lua/plenary.nvim" },
 		{ "nvim-telescope/telescope-media-files.nvim" },
 		{ "nvim-telescope/telescope-ui-select.nvim" },
 		{ "nvim-tree/nvim-web-devicons" },
 	},
-	init = function()
-		require("telescope").load_extension("media_files")
-	end,
 }
