cd2c29a56523a19a963a97f02bf9fe4d658e636c
Andrew Konoplia <andrew.konoplia@enra.co.uk>
2024-08-13 18:22:10 +0300

    expand for virtual text

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -46,6 +46,9 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		--  For example, in C this would take you to the header
 		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+		-- Expand virtual text
+		map("<leader>e", vim.diagnostic.open_float, "[E]xpand virtual text")
+
 		-- The following two autocommands are used to highlight references of the
 		-- word under your cursor when your cursor rests there for a little while.
 		--    See `:help CursorHold` for information about when this is executed
