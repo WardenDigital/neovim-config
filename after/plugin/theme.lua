a45d04a6cf96a99d114badc9e9dddf33e622d86f
WardenDigital <wardendigital@proton.me>
2025-01-23 18:16:03 +0100

    updated styles

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -2,5 +2,14 @@
 require("catppuccin").setup({
 	flavour = "mocha", -- latte, frappe, macchiato, mocha
     transparent_background = true,
+    styles = {
+        comments = {},
+        conditionals = {},
+    }
 })
+
+require("notify").setup({
+  background_colour = "#000000",
+})
+
 vim.cmd("colorscheme catppuccin")
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,9 +6,6 @@ return {
             "williamboman/mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             "WhoIsSethDaniel/mason-tool-installer.nvim",
-            --  Useful status updates for LSP.
-            --  NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-            { "j-hui/fidget.nvim", opts = {} },
         },
     },
     { "hrsh7th/cmp-nvim-lsp" },
