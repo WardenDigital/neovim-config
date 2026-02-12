9b18f1b61e2b09bf6c5cda3db2e51b6544f3c85a
WardenDigital <wardendigital@proton.me>
2026-02-13 00:46:51 +0100

    dbee instad of dadbod

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,19 +1,14 @@
--- Database
 return {
-	{ "tpope/vim-dadbod" },
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBUffer",
-		},
-		init = function()
-			vim.g.db_ui_use_nerd_fonts = 1
-		end,
-	},
+    {
+        "kndndrj/nvim-dbee",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+        },
+        build = function()
+            require("dbee").install()
+        end,
+        config = function()
+            require("dbee").setup( --[[optional config]])
+        end,
+    },
 }
