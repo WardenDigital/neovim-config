66f35d19eb8db5cde55aae77b62929616349113c
WardenDigital <wardendigital@proton.me>
2026-02-23 21:22:24 +0100

    fix ts and vue ls

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,110 +2,131 @@
 local picker = require("snacks").picker
 
 vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
-    callback = function(event)
-        local map = function(keys, func, desc)
-            vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
-        end
+	group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+	callback = function(event)
+		local map = function(keys, func, desc)
+			vim.keymap.set("n", keys, func, { buffer = event.buf, desc = "LSP: " .. desc })
+		end
 
-        -- Jump to the definition of the word under your cursor.
-        --  This is where a variable was first declared, or where a function is defined, etc.
-        --  To jump back, press <C-T>.
-        map("gd", picker.lsp_definitions, "[G]oto [D]efinition")
+		-- Jump to the definition of the word under your cursor.
+		--  This is where a variable was first declared, or where a function is defined, etc.
+		--  To jump back, press <C-T>.
+		map("gd", picker.lsp_definitions, "[G]oto [D]efinition")
 
-        -- Find references for the word under your cursor.
-        map("gr", picker.lsp_references, "[G]oto [R]eferences")
+		-- Find references for the word under your cursor.
+		map("gr", picker.lsp_references, "[G]oto [R]eferences")
 
-        -- Jump to the implementation of the word under your cursor.
-        --  Useful when your language has ways of declaring types without an actual implementation.
-        map("gi", picker.lsp_implementations, "[G]oto [I]mplementation")
+		-- Jump to the implementation of the word under your cursor.
+		--  Useful when your language has ways of declaring types without an actual implementation.
+		map("gi", picker.lsp_implementations, "[G]oto [I]mplementation")
 
-        -- Jump to the type of the word under your cursor.
-        --  Useful when you're not sure what type a variable is and you want to see
-        --  the definition of its *type*, not where it was *defined*.
-        map("<leader>D", picker.lsp_type_definitions, "Type [D]efinition")
+		-- Jump to the type of the word under your cursor.
+		--  Useful when you're not sure what type a variable is and you want to see
+		--  the definition of its *type*, not where it was *defined*.
+		map("<leader>D", picker.lsp_type_definitions, "Type [D]efinition")
 
-        -- Fuzzy find all the symbols in your current document.
-        --  Symbols are things like variables, functions, types, etc.
-        map("<leader>ds", picker.lsp_symbols, "[D]ocument [S]ymbols")
+		-- Fuzzy find all the symbols in your current document.
+		--  Symbols are things like variables, functions, types, etc.
+		map("<leader>ds", picker.lsp_symbols, "[D]ocument [S]ymbols")
 
-        -- Fuzzy find all the symbols in your current workspace
-        --  Similar to document symbols, except searches over your whole project.
-        map("<leader>ws", picker.lsp_workspace_symbols, "[W]orkspace [S]ymbols")
+		-- Fuzzy find all the symbols in your current workspace
+		--  Similar to document symbols, except searches over your whole project.
+		map("<leader>ws", picker.lsp_workspace_symbols, "[W]orkspace [S]ymbols")
 
-        -- Rename the variable under your cursor
-        --  Most Language Servers support renaming across files, etc.
-        map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+		-- Rename the variable under your cursor
+		--  Most Language Servers support renaming across files, etc.
+		map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-        -- Execute a code action, usually your cursor needs to be on top of an error
-        -- or a suggestion from your LSP for this to activate.
-        map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+		-- Execute a code action, usually your cursor needs to be on top of an error
+		-- or a suggestion from your LSP for this to activate.
+		map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-        -- Opens a popup that displays documentation about the word under your cursor
-        --  See `:help K` for why this keymap
-        map("K", ":Lspsaga hover_doc<CR>", "Hover Documentation")
+		-- Opens a popup that displays documentation about the word under your cursor
+		--  See `:help K` for why this keymap
+		map("K", ":Lspsaga hover_doc<CR>", "Hover Documentation")
 
-        -- WARN: This is not Goto Definition, this is Goto Declaration.
-        --  For example, in C this would take you to the header
-        map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
+		-- WARN: This is not Goto Definition, this is Goto Declaration.
+		--  For example, in C this would take you to the header
+		map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
-        -- Expand virtual text
-        map("<leader>e", vim.diagnostic.open_float, "[E]xpand virtual text")
+		-- Expand virtual text
+		map("<leader>e", vim.diagnostic.open_float, "[E]xpand virtual text")
 
-        -- Get and outline
-        map("<leader>o", ":Lspsaga outline<CR>", "[O]utline")
+		-- Get and outline
+		map("<leader>o", ":Lspsaga outline<CR>", "[O]utline")
 
-        -- The following two autocommands are used to highlight references of the
-        -- word under your cursor when your cursor rests there for a little while.
-        --    See `:help CursorHold` for information about when this is executed
-        --
-        -- When you move your cursor, the highlights will be cleared (the second autocommand).
-        local client = vim.lsp.get_client_by_id(event.data.client_id)
-        if client and client.server_capabilities.documentHighlightProvider then
-            vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-                buffer = event.buf,
-                callback = vim.lsp.buf.document_highlight,
-            })
+		-- The following two autocommands are used to highlight references of the
+		-- word under your cursor when your cursor rests there for a little while.
+		--    See `:help CursorHold` for information about when this is executed
+		--
+		-- When you move your cursor, the highlights will be cleared (the second autocommand).
+		local client = vim.lsp.get_client_by_id(event.data.client_id)
+		if client and client.server_capabilities.documentHighlightProvider then
+			vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+				buffer = event.buf,
+				callback = vim.lsp.buf.document_highlight,
+			})
 
-            vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-                buffer = event.buf,
-                callback = vim.lsp.buf.clear_references,
-            })
-        end
-    end,
+			vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+				buffer = event.buf,
+				callback = vim.lsp.buf.clear_references,
+			})
+		end
+	end,
 })
 
+-- TS and Vue, cause they are "special"
+local vue_language_server_path = vim.fn.expand("$MASON/packages")
+	.. "/vue-language-server"
+	.. "/node_modules/@vue/language-server"
+local tsserver_filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" }
+local vue_plugin = {
+	name = "@vue/typescript-plugin",
+	location = vue_language_server_path,
+	languages = { "vue" },
+	configNamespace = "typescript",
+}
+
+local ts_ls_config = {
+	init_options = {
+		plugins = {
+			vue_plugin,
+		},
+	},
+	filetypes = tsserver_filetypes,
+}
 local servers = {
-    qmlls = {
-        cmd = { "qmlls6" },
-        root_markers = { "shell.qml", ".qmlls.ini" },
-    },
-    vue_ls = {},
-    lua_ls = {
-        settings = {
-            Lua = {
-                runtime = { version = "LuaJIT" },
-                workspace = {
-                    checkThirdParty = false,
-                    -- Tells lua_ls where to find all the Lua files that you have loaded
-                    -- for your neovim configuration.
-                    library = {
-                        "${3rd}/luv/library",
-                        unpack(vim.api.nvim_get_runtime_file("", true)),
-                    },
-                    -- If lua_ls is really slow on your computer, you can try this instead:
-                    -- library = { vim.env.VIMRUNTIME },
-                },
-                completion = {
-                    callSnippet = "Replace",
-                },
-                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-                -- diagnostics = { disable = { 'missing-fields' } },
-            },
-        },
-    },
+	qmlls = {
+		cmd = { "qmlls6" },
+		root_markers = { "shell.qml", ".qmlls.ini" },
+	},
+	vue_ls = {},
+	ts_ls = ts_ls_config,
+	lua_ls = {
+		settings = {
+			Lua = {
+				runtime = { version = "LuaJIT" },
+				workspace = {
+					checkThirdParty = false,
+					-- Tells lua_ls where to find all the Lua files that you have loaded
+					-- for your neovim configuration.
+					library = {
+						"${3rd}/luv/library",
+						unpack(vim.api.nvim_get_runtime_file("", true)),
+					},
+					-- If lua_ls is really slow on your computer, you can try this instead:
+					-- library = { vim.env.VIMRUNTIME },
+				},
+				completion = {
+					callSnippet = "Replace",
+				},
+				-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+				-- diagnostics = { disable = { 'missing-fields' } },
+			},
+		},
+	},
 }
 
 for k, t in pairs(servers) do
-    vim.lsp.config(k, t)
+	vim.lsp.config(k, t)
 end
